6e98b644031737e116e0bbd86f478f71e14b3412
michelwene <michelwene@hotmail.com>
2025-11-23 09:45:10 -0300

    chore: main arquivos de configuração no frontend
    Adicionamos os arquivos .npmrc, .nvmrc, .prettierrc e extensions.json do VSCode ao Frontend para melhorar a experiência do desenvolvedor e a consistência. Atualizamos o arquivo .env.example com uma URL de API padrão, expandimos as regras do .gitignore e aprimoramos a configuração do ESLint com novos plugins e regras mais rigorosas. Atualizamos as dependências e devDependencies do package.json para usar as versões exatas e alteramos as URLs de registro para o Azure DevOps. O arquivo .gitignore do Backend agora ignora a saída do diretório dist.

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -18,6 +20,145 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+      'simple-import-sort': simpleImportSort,
+      import: importPlugin,
+      'react-hooks': reactHooks,
+    },
+
+    rules: {
+      // === eslint ====
+      'no-console': ['warn', { allow: ['error'] }],
+      'no-warning-comments': ['warn'],
+
+      // === react ====
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // === @typescript-eslint ====
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-exports': [
+        'warn',
+        {
+          'fixMixedExportsWithInlineTypeSpecifier': true,
+        },
+      ],
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        {
+          'assertionStyle': 'never',
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          'fixStyle': 'separate-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-import-type-side-effects': 'error',
+      '@typescript-eslint/naming-convention': [
+        'off',
+        {
+          'selector': 'typeLike',
+          'format': ['StrictPascalCase'],
+        },
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          'checksVoidReturn': {
+            'attributes': false,
+          },
+        },
+      ],
+      '@typescript-eslint/prefer-nullish-coalescing': [
+        'error',
+        { ignorePrimitives: { string: true } },
+      ],
+      '@typescript-eslint/prefer-ts-expect-error': 'error',
+
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          'groups': [
+            ['^react(?:/.*)?$'],
+            ['^next(?:/.*)?$'],
+            ['^[a-zA-Z@]'],
+            ['^~'],
+            ['^\\.\\.'],
+            ['^\\.'],
+            ['^\\u0000'],
+            ['.*\\.css$'],
+            ['^'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+
+      // === unicorn ====
+      'unicorn/consistent-empty-array-spread': 'error',
+      'unicorn/consistent-existence-index-check': 'error',
+      'unicorn/consistent-function-scoping': 'error',
+      'unicorn/new-for-builtins': 'error',
+      'unicorn/no-lonely-if': 'error',
+      'unicorn/no-new-array': 'error',
+      'unicorn/no-new-buffer': 'error',
+      'unicorn/no-unreadable-iife': 'error',
+      'unicorn/no-useless-spread': 'error',
+      'unicorn/prefer-add-event-listener': 'error',
+      'unicorn/prefer-array-find': 'error',
+      'unicorn/prefer-array-flat': 'error',
+      'unicorn/prefer-array-flat-map': 'error',
+      'unicorn/prefer-array-index-of': 'error',
+      'unicorn/prefer-array-some': 'error',
+      'unicorn/prefer-at': 'error',
+      'unicorn/prefer-blob-reading-methods': 'error',
+      'unicorn/prefer-code-point': 'error',
+      'unicorn/prefer-date-now': 'error',
+      'unicorn/prefer-default-parameters': 'error',
+      'unicorn/prefer-dom-node-append': 'error',
+      'unicorn/prefer-dom-node-dataset': 'error',
+      'unicorn/prefer-dom-node-remove': 'error',
+      'unicorn/prefer-dom-node-text-content': 'error',
+      'unicorn/prefer-event-target': 'error',
+      'unicorn/prefer-export-from': 'error',
+      'unicorn/prefer-global-this': 'error',
+      'unicorn/prefer-includes': 'error',
+      'unicorn/prefer-keyboard-event-key': 'error',
+      'unicorn/prefer-logical-operator-over-ternary': 'error',
+      'unicorn/prefer-math-min-max': 'error',
+      'unicorn/prefer-math-trunc': 'error',
+      'unicorn/prefer-modern-dom-apis': 'error',
+      'unicorn/prefer-modern-math-apis': 'error',
+      'unicorn/prefer-module': 'error',
+      'unicorn/prefer-native-coercion-functions': 'error',
+      'unicorn/prefer-negative-index': 'error',
+      'unicorn/prefer-node-protocol': 'error',
+      'unicorn/prefer-number-properties': 'error',
+      'unicorn/prefer-object-from-entries': 'error',
+      'unicorn/prefer-optional-catch-binding': 'error',
+      'unicorn/prefer-prototype-methods': 'error',
+      'unicorn/prefer-query-selector': 'error',
+      'unicorn/prefer-reflect-apply': 'error',
+      'unicorn/prefer-regexp-test': 'error',
+      'unicorn/prefer-set-has': 'error',
+      'unicorn/prefer-set-size': 'error',
+      'unicorn/prefer-spread': 'error',
+      'unicorn/prefer-string-replace-all': 'error',
+      'unicorn/prefer-string-slice': 'error',
+      'unicorn/prefer-string-starts-ends-with': 'error',
+      'unicorn/prefer-string-trim-start-end': 'error',
+      'unicorn/prefer-structured-clone': 'error',
+      'unicorn/prefer-switch': 'error',
+      'unicorn/prefer-ternary': 'error',
+      'unicorn/prefer-top-level-await': 'error',
+      'unicorn/prefer-type-error': 'error',
+      'unicorn/throw-new-error': 'error',
     },
   },
 ])
--- Frontend/src/routes/routes.tsx
+++ Frontend/src/routes/routes.tsx
@@ -1,143 +1,145 @@
-import MetasPage from '../pages/Profissional/Metas/Metas';
-import { Routes, Route } from 'react-router-dom';
-import ProtectedRoute from '../components/ProtectedRoute';
+import { Route, Routes } from 'react-router-dom'
+import ProtectedRoute from '../components/ProtectedRoute'
+import MetasPage from '../pages/Profissional/Metas/Metas'
 
 // Páginas públicas
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
+import Home from '../pages/Home'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 // Páginas do Profissional
-import Dashboard from '../pages/Profissional/Dashboard/Dashboard';
-import CadastrarCriancas from '../pages/Profissional/CadastrarCriancas/CadastrarCriancas';
-import VerDetalhesCriancaCadastrada from '../pages/Profissional/CadastrarCriancas/VerDetalhesCriancaCadastrada';
-import EditarCriancaCadastrada from '../pages/Profissional/CadastrarCriancas/EditarCriancaCadastrada';
-import Profissionais from '../pages/Profissional/Profissionais/Profissionais';
-import PerfilProfissional from '../pages/Profissional/Perfil/VerPerfil';
-import PerfilEdit from '../pages/Profissional/Perfil/EditarPerfil';
-
-
-
+import CadastrarCriancas from '../pages/Profissional/CadastrarCriancas/CadastrarCriancas'
+import EditarCriancaCadastrada from '../pages/Profissional/CadastrarCriancas/EditarCriancaCadastrada'
+import VerDetalhesCriancaCadastrada from '../pages/Profissional/CadastrarCriancas/VerDetalhesCriancaCadastrada'
+import Dashboard from '../pages/Profissional/Dashboard/Dashboard'
+import PerfilEdit from '../pages/Profissional/Perfil/EditarPerfil'
+import PerfilProfissional from '../pages/Profissional/Perfil/VerPerfil'
+import Profissionais from '../pages/Profissional/Profissionais/Profissionais'
 
 export default function AppRoutes() {
   return (
     <Routes>
       {/* Rotas Públicas */}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route
+        path='/'
+        element={<Home />}
+      />
+      <Route
+        path='/login'
+        element={<Login />}
+      />
+      <Route
+        path='/register'
+        element={<Register />}
+      />
 
       {/* Rotas do Profissional */}
-      <Route 
-        path="/profissional/dashboard" 
+      <Route
+        path='/profissional/dashboard'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <Dashboard />
           </ProtectedRoute>
-        } 
+        }
       />
-      <Route 
-        path="/profissional/criancas" 
+      <Route
+        path='/profissional/criancas'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <CadastrarCriancas />
           </ProtectedRoute>
-        } 
+        }
       />
-      <Route 
-        path="/profissional/criancas/detalhes/:id" 
+      <Route
+        path='/profissional/criancas/detalhes/:id'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <VerDetalhesCriancaCadastrada />
           </ProtectedRoute>
-        } 
+        }
       />
-      <Route 
-        path="/profissional/criancas/editar/:id" 
+      <Route
+        path='/profissional/criancas/editar/:id'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <EditarCriancaCadastrada />
           </ProtectedRoute>
-        } 
+        }
       />
 
-      <Route 
-        path="/profissional/profissionais" 
+      <Route
+        path='/profissional/profissionais'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <Profissionais />
           </ProtectedRoute>
-        } 
+        }
       />
 
-
-      <Route 
-        path="/profissional/perfil" 
+      <Route
+        path='/profissional/perfil'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <PerfilProfissional />
           </ProtectedRoute>
-        } 
+        }
       />
 
-      <Route 
-        path="/profissional/perfil/:id" 
+      <Route
+        path='/profissional/perfil/:id'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <PerfilProfissional />
           </ProtectedRoute>
-        } 
+        }
       />
 
-      <Route 
-        path="/profissional/perfil/:id" 
+      <Route
+        path='/profissional/perfil/:id'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <PerfilProfissional />
           </ProtectedRoute>
-        } 
+        }
       />
 
-
-
-      <Route 
-        path="/profissional/perfil/editar" 
+      <Route
+        path='/profissional/perfil/editar'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <PerfilEdit />
           </ProtectedRoute>
-        } 
+        }
       />
 
-       <Route 
-        path="/profissional/metas" 
+      <Route
+        path='/profissional/metas'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL']}>
             <MetasPage />
           </ProtectedRoute>
-        } 
+        }
       />
 
       {/* Rotas do Responsável */}
-      <Route 
-        path="/responsavel/dashboard" 
+      <Route
+        path='/responsavel/dashboard'
         element={
           <ProtectedRoute allowedRoles={['RESPONSAVEL']}>
             <Dashboard />
           </ProtectedRoute>
-        } 
+        }
       />
 
-
       {/* Rota de compatibilidade */}
-      <Route 
-        path="/dashboard" 
+      <Route
+        path='/dashboard'
         element={
           <ProtectedRoute allowedRoles={['PROFISSIONAL', 'RESPONSAVEL']}>
             <Dashboard />
           </ProtectedRoute>
-        } 
+        }
       />
     </Routes>
-  );
+  )
 }
